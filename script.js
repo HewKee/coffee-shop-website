2cdc27699553e6393852d89ad80df5ceef2c59d3
HewKee <120575229+HewKee@users.noreply.github.com>
2022-12-14 21:50:59 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+let navbar = document.querySelector('.navbar')
+
+document.querySelector('#menu-btn').onclik = () =>{
+    navbar.classList.toggle('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let searchForm = document.querySelector('.search-form');
+
+document.querySelector('#search-btn').onclik = () =>{
+    searchForm.classList.toggle('active');
+    navbar.classList.remove('active');
+    cartItem.classList.remove('active');
+}
+
+let cartItem = document.querySelector('.cart-items-container');
+
+document.querySelector('#cart-btn').onclik = () =>{
+    cartItem.classList.toggle('active');
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+}
+
+window.onscroll = () =>{
+    navbar.classList.remove('active');
+    searchForm.classList.remove('active');
+    cartItem.classList.remove('active');
+}
